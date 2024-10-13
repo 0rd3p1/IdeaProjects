9e43f30e51f79dd9ae4748e25c8fbc102192e445
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2024-10-13 20:20:43 -0300

    13/10/24

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String n;
+
+        System.out.print("Qual e o seu Nome? ");
+        n = sc.nextLine();
+
+        System.out.println("Ola " + n + ", seja bem vindo!");
 
     }
 }
