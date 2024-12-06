1decf61ff324598469a1a23bdd10ed5f7de93fff
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2024-12-06 19:21:10 -0300

    06/12/24

--- ProjetoIntegradorFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projetointegradorfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projetointegradorfx to javafx.fxml;
-    exports com.example.projetointegradorfx;
-}
