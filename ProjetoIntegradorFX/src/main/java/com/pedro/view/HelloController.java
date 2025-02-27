701ffef51276ed637f95307f3e2a473dc7c5a28a
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2025-02-26 23:31:28 -0300

    Laptop

---
+++ ProjetoIntegradorFX/src/main/java/com/pedro/view/HelloController.java
@@ -0,0 +1,14 @@
+package com.pedro.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
